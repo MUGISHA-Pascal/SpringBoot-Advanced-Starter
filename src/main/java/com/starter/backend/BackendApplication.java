89d2418c29466ce365ac4826583cb0f24bc69499
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-04-07 21:55:51 +0200

    chore : restructuring file structure

--- src/main/java/com/starter/backend/BackendApplication.java
+++ src/main/java/com/starter/backend/BackendApplication.java
@@ -1,13 +1,19 @@
 package com.starter.backend;
 
+import com.starter.backend.components.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+
+		ConfigurableApplicationContext cxt = SpringApplication.run(BackendApplication.class, args);
+		Student st = cxt.getBean(Student.class);
+		Student dt = cxt.getBean(Student.class);
+		System.out.println("Application Started...");
 	}
 
 }
