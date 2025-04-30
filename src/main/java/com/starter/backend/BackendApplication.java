b8e038d29382524952db366bfd84f3fe16b97fe2
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-04-30 20:26:29 +0200

    feat : adding enums and a model

--- src/main/java/com/starter/backend/BackendApplication.java
+++ src/main/java/com/starter/backend/BackendApplication.java
@@ -1,19 +1,12 @@
 package com.starter.backend;
 
-import com.starter.backend.components.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
-
-		ConfigurableApplicationContext cxt = SpringApplication.run(BackendApplication.class, args);
-		Student st = cxt.getBean(Student.class);
-		Student dt = cxt.getBean(Student.class);
-		System.out.println("Application Started...");
+		SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
