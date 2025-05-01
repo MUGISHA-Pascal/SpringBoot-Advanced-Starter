41f9f2b4c4ef48e7cc6bd3f746098e1404730cb7
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-05-01 13:42:31 +0200

    feat : adding the payloads

--- src/main/java/com/starter/backend/config/SwaggerApiDoc.java
+++ src/main/java/com/starter/backend/config/SwaggerApiDoc.java
@@ -1,4 +1,4 @@
-package com.starter.backend.controllers;
+package com.starter.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
