0f803e41e8ef77742d8e5d7d0b806f78397d86b0
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-05-05 09:40:19 +0200

    feat : adding secutity utilities

---
+++ src/main/java/com/starter/backend/controllers/AuthController.java
@@ -0,0 +1,16 @@
+package com.starter.backend.controllers;
+
+import com.starter.backend.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private AuthenticationManager authenticationManeger;
+}
