aa311e994abdc891a4dc5d90a550e8b80a75e2f4
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-05-06 14:36:14 +0200

    fix : swagger documentation

--- src/main/java/com/starter/backend/config/WebSecurityConfig.java
+++ src/main/java/com/starter/backend/config/WebSecurityConfig.java
@@ -77,6 +77,7 @@ public class WebSecurityConfig {
                                 new AntPathRequestMatcher("/**/*.css"),
                                 new AntPathRequestMatcher("/**/*.js"),
                                 new AntPathRequestMatcher("/v2/api-docs"),
+                                new AntPathRequestMatcher("/v3/api-docs/**"),
                                 new AntPathRequestMatcher("/configuration/ui"),
                                 new AntPathRequestMatcher("/swagger-resources/**"),
                                 new AntPathRequestMatcher("/configuration/security"),
