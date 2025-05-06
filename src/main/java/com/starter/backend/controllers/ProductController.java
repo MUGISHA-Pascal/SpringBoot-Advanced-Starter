e76232858ad5574d8c88e16025b51ba99029457b
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-05-06 13:43:13 +0200

    fix : sort by column

--- src/main/java/com/starter/backend/controllers/ProductController.java
+++ src/main/java/com/starter/backend/controllers/ProductController.java
@@ -3,7 +3,9 @@ package com.starter.backend.controllers;
 import com.starter.backend.dtos.ProductDto;
 import com.starter.backend.models.Product;
 import com.starter.backend.services.ProductService;
+import com.starter.backend.util.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,6 +22,10 @@ public class ProductController {
         this.productService = productService;
     }
 
+    @GetMapping("/order")
+    public Page<Product> getPaginatedAndSortedProducts(@RequestParam(value = "page" ,defaultValue = Constants.DEFAULT_PAGE_NUMBER) int page, @RequestParam(value = "size",defaultValue = Constants.DEFAULT_PAGE_SIZE) int size, @RequestParam(value = "column") String column){
+        return productService.productsPagination(page,size,column);
+    }
     @GetMapping
     public List<Product> getAllProducts(){
         return productService.getAllProducts();
--- src/main/java/com/starter/backend/services/ProductService.java
+++ src/main/java/com/starter/backend/services/ProductService.java
@@ -4,7 +4,12 @@ import com.starter.backend.dtos.ProductDto;
 import com.starter.backend.exceptions.ApiRequestException;
 import com.starter.backend.models.Product;
 import com.starter.backend.repository.ProductRepository;
+import com.starter.backend.util.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -58,6 +63,12 @@ public class ProductService {
     }
     public List<Product> findProductsByCategory(String category){
         List<Product> products = productRepository.findByCategory(category);
+        return products;
+    }
+    public Page<Product> productsPagination(int page,int size,String column){
+        Constants.validatePageNumberAndPageSize(page,size);
+        Pageable pageable =(Pageable) PageRequest.of(page,size, Sort.Direction.ASC,column);
+        Page<Product> products = productRepository.findAll(pageable);
         return products;
     }
 }
