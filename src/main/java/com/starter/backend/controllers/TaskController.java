89c28d14624bca3d22a2be69ce90f2000844a256
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-05-07 20:47:33 +0200

    update adding the task resolvers

---
+++ src/main/java/com/starter/backend/controllers/TaskController.java
@@ -0,0 +1,39 @@
+package com.starter.backend.controllers;
+
+import com.starter.backend.dtos.TaskDto;
+import com.starter.backend.models.Task;
+import com.starter.backend.services.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+public class TaskController {
+    @Autowired
+    private TaskService taskService;
+    @QueryMapping
+    public List<Task> getAllTasks(){
+        return taskService.getAllTasks();
+    }
+    @MutationMapping
+    public Task addTask(@Argument("input") TaskDto task){
+         try{
+             return taskService.addTask(task);}catch (Exception e){
+            System.out.println("-----"+e.getMessage()+"-----");
+         }
+         return null;
+    }
+    @MutationMapping
+    public Task updateTask(@Argument("input") TaskDto task, @Argument("id")UUID id){
+        return taskService.updateTask(task,id);
+    }
+    @MutationMapping
+    public String deleteTask(@Argument("id") UUID id){
+        return taskService.deleteTask(id);
+    }
+}
